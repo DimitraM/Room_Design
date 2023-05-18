e873be888ae04b9b7a81e16835fb4208e34fbeaa
Andreas-Ts <119705172+Andreas-Ts@users.noreply.github.com>
2023-05-18 17:18:26 +0300

    Project φτιαχμένο με intelij idea με την αναζήτηση των τύπων δωματίου φτιαχμένο (Οι κλάσσεις είναι σε πολύ προτώγονο στάδιο ακόμα)

---
+++ Παραδοτέο 4/Project code 0.1 Vanilia/Room Design 0.1/src/Enums/RoomType.java
@@ -0,0 +1,18 @@
+package Enums;
+
+public enum RoomType {
+    bedroom(0),kitchen(0),toilet(0),livingroom(1),hall(1),
+    corridor(1),balcony(1),studio(2),garden(2);
+
+    private final int priority;
+
+    RoomType(int priority){
+        this.priority=priority;
+    }
+
+    public int getPriority(){return priority;}
+
+
+
+
+}
