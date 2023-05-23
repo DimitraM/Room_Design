27a959f434af06f2fd89ed8050f3ed7f865011d8
Andreas-Ts <119705172+Andreas-Ts@users.noreply.github.com>
2023-05-23 22:51:57 +0300

    Update RoomType.java

--- Παραδοτέο 5/Project code 0.1 Vanilia/Room Design 0.1/src/Enums/RoomType.java
+++ Παραδοτέο 5/Project code 0.1 Vanilia/Room Design 0.1/src/Enums/RoomType.java
@@ -1,13 +1,18 @@
 package Enums;
 
 public enum RoomType {
-    bedroom(0),kitchen(0),toilet(0),livingroom(1),hall(1),
-    corridor(1),balcony(1),studio(2),garden(2);
+    bedroom(0,new double[]{3.5,4},new [12,15],[12,15]);//kitchen(0),toilet(0),livingroom(1),hall(1),
+   // corridor(1),balcony(1),studio(2),garden(2);
 
     private final int priority;
-
-    RoomType(int priority){
+    private final double[] heightRecommendations;
+    private final double[] widthRecommendations;
+    private final float[] lenghttRecommendations;
+    RoomType(int priority,float[] heightRecommendations,float[] widthRecommendations,float[] lenghttRecommendations){
         this.priority=priority;
+        this.widthRecommendations=widthRecommendations;
+        this.heightRecommendations=heightRecommendations;
+        this.lenghttRecommendations=lenghttRecommendations;
     }
 
     public int getPriority(){return priority;}
