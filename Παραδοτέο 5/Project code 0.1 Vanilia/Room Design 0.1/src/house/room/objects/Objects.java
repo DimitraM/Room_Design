86ce46639ee916764c197a041c1a184962dab944
mira <miraslamaj@gmail.com>
2023-05-27 16:23:23 +0300

    Abstract object class

--- Παραδοτέο 5/Project code 0.1 Vanilia/Room Design 0.1/src/house/room/objects/Objects.java
+++ Παραδοτέο 5/Project code 0.1 Vanilia/Room Design 0.1/src/house/room/objects/Objects.java
@@ -1,5 +1,18 @@
 package house.room.objects;
 
-public class Objects {
+abstract class Objects {
+    private float[] position = new float[3]; // position of an object is
+    // consisted of x, y and z axis values
 
+    private float height;
+    private float width;
+    private float length;
+
+    public abstract Object choose_object();
+    public abstract void post_restrictions();
+    public abstract void check_rooms_objects();
+    public abstract void change_furn_loc();
+
+    public abstract void saveChanges();
+    public abstract void deleteChanges();
 }
