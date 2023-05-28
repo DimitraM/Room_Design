31d5551681da328bead17074e1061028c7076805
Andreas-Ts <119705172+Andreas-Ts@users.noreply.github.com>
2023-05-28 22:37:54 +0300

    Συντονισμός αρχείων

--- Παραδοτέο 5/Project code 0.1 Vanilia/Room Design 0.1/src/code/house/room/Room.java
+++ Παραδοτέο 5/Project code 0.1 Vanilia/Room Design 0.1/src/code/house/room/Room.java
@@ -9,16 +9,18 @@ public class Room {
     private ArrayList<RoomType> type;
     private String name;
 
-    public Room(House house,ArrayList<RoomType> type) {
+    public Room(House house, ArrayList<RoomType> type) {
 
 
-        this.house-house;
+        this.house - house;
         this.type = type;
         items = new ArrayList<Furniture>();
         doors = new ArrayList<Openings>();
     }
 
-    public ArrayList<RoomType> getRoomType(){return  type;}
+    public ArrayList<RoomType> getRoomType() {
+        return type;
+    }
 
     public String getName() {
         return name;
@@ -28,21 +30,25 @@ public class Room {
         this.name = name;
     }
 
-    public void addItems(Furniture furniture)
-    {
+    public void addItems(Furniture furniture) {
         items.add(furniture);
     }
 
-    public void addOpenings(Openings opening)
-    {
+    public void addOpenings(Openings opening) {
         doors.add(opening);
     }
 
-    public void sortOptions(RoomType type){
+    public void sortOptions(RoomType type) {
         //this method sorts options in list for user to add
-        if(doors==null){
+        if (doors == null) {
             //show only openings firsts
         }
 
 
     }
+    public ArrayList<Wall> getWalls(){
+
+        return walls;
+
+    }
+}
