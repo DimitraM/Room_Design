402021e8c047681ba05c4868f338d3dc414d2817
Andreas-Ts <119705172+Andreas-Ts@users.noreply.github.com>
2023-06-10 19:14:15 +0300

    Μικρές αλλαγές για να εμπεριέχει οι classes wall και corner το δωμάτιο στο οποίο ανήκουν. Πρόσθεση στο contructor

--- Παραδοτέο 5/Project code 0.1 Vanilia/Room Design 0.1/src/code/house/room/objects/Corner.java
+++ Παραδοτέο 5/Project code 0.1 Vanilia/Room Design 0.1/src/code/house/room/objects/Corner.java
@@ -5,19 +5,19 @@ import code.house.room.Room;
 import java.util.ArrayList;
 
 public class Corner extends Objects{
-    private Room room;
+
     private ArrayList<Wall> walls;
-    public Corner(float length,float width){
 
-        super(length,width,0);
+    public Corner(Room room,float length,float width){
+
+        super(room,length,width,0);
         walls.add(new Wall(this.room,"white","concrete","none",this));
-
+        room.addCorner(this);
     }
-    public Corner(float length,float width,Wall wall){
-
-        room=wall.getRoom();
-        super(length,width,0);
+    public Corner(Room room,float length,float width,Wall wall){
+        super(room,length,width,0);
         walls.add(wall);
+        room.addCorner(this);
     }
 
     public boolean isNotFull(){
